713e133124cccc0bbe7cbc1445a4490f47fec7de
James Coliz <jcoliz@xbox.com>
2017-03-31 13:40:51 -0700

    New project for automation hat class library

---
+++ Pimoroni.MsIot.AutomationHat/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pimoroni.MsIot.AutomationHat
+{
+    public class Class1
+    {
+    }
+}
