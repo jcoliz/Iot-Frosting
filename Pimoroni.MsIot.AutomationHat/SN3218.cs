0156deda05d8e606a3552a6d137e55c17a89a8ce
James Coliz <jcoliz@xbox.com>
2017-04-01 16:21:13 -0700

    Initial stub class

---
+++ Pimoroni.MsIot.AutomationHat/SN3218.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pimoroni.MsIot
+{
+    /// <summary>
+    /// Control an SN3218 18-port LED driver
+    /// </summary>
+    /// <remarks>
+    /// Influenced by:
+    /// https://github.com/pimoroni/sn3218/blob/master/library/sn3218.py
+    /// and
+    /// https://github.com/ms-iot/samples/blob/develop/I2cPortExpander/CS/MainPage.xaml.cs
+    /// </remarks>
+
+    public class SN3218
+    {
+    }
+}
