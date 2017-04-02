3b16f061793adb4f8da7393c231dafcde3764a15
James Coliz <jcoliz@xbox.com>
2017-04-01 17:02:44 -0700

    Implemented IDisposable on SN3218, becuse I2C device needs to be disposed.

--- Pimoroni.MsIot.AutomationHat/SN3218.cs
+++ Pimoroni.MsIot.AutomationHat/SN3218.cs
@@ -17,7 +17,7 @@ namespace Pimoroni.MsIot
     /// https://github.com/ms-iot/samples/blob/develop/I2cPortExpander/CS/MainPage.xaml.cs
     /// </remarks>
 
-    public class SN3218
+    public class SN3218: IDisposable
     {
         // use these constants for controlling how the I2C bus is setup
         private const byte I2C_ADDRESS = 0x54;
@@ -85,5 +85,40 @@ namespace Pimoroni.MsIot
                 Output(values);
             }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Device.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~SN3218() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
